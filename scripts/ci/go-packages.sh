8a6f00d312623edbccfdf49a2f79d5c6b9715cf1
Albert Lee <albertlee166@gmail.com>
2026-05-21 11:39:36 +0800

    feat: P2A-003 - Keep Heavy Go Data Suites out of Fast Unit Gates

--- scripts/ci/go-packages.sh
+++ scripts/ci/go-packages.sh
@@ -9,8 +9,11 @@ list_packages() {
 		"--build")
 			go list -e -f '{{if or .GoFiles .CgoFiles}}{{.ImportPath}}{{end}}' ./...
 			;;
+		"--acceptance-data")
+			go list -e ./test/chinook ./test/northwind
+			;;
 		*)
-			echo "usage: $0 [--test|--build]" >&2
+			echo "usage: $0 [--test|--build|--acceptance-data]" >&2
 			exit 2
 			;;
 	esac
@@ -18,4 +21,15 @@ list_packages() {
 
 # `go list ./...` traverses installed web dependencies when web/node_modules
 # exists locally. Keep all-package Go gates scoped to repository packages.
-list_packages "${1:-}" | awk 'NF && $0 !~ /(^|\/)node_modules(\/|$)/ { print }'
+#
+# The Chinook and Northwind suites are full data acceptance checks. Keep the
+# default unit/race/coverage package list fast; run them explicitly through
+# `--acceptance-data` or `make test-data-acceptance`.
+case "${1:-}" in
+	"--acceptance-data")
+		list_packages "$1" | awk 'NF && $0 !~ /(^|\/)node_modules(\/|$)/ { print }'
+		;;
+	*)
+		list_packages "${1:-}" | awk 'NF && $0 !~ /(^|\/)node_modules(\/|$)/ && $0 !~ /\/test\/(chinook|northwind)$/ { print }'
+		;;
+esac
